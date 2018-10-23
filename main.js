721df2a8dabca6351e4d54365e0755fe390ad130
onlymx13 <onlymx13@gmail.com>
2018-10-23 12:30:21 -0500

    Mouse stuff

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 'use strict';
-var level, context;
+var level, context, pirates, click;
+mouseClick = false;
 document.onload=function () {
-context = document.getElementById('canvas').getContext('2d');
+var canvas = document.getElementById('canvas');
+canvas.addEventListener('mouseDown', mouseClick, false);
+canvas.addEventListener('touchstart', touchClick, false);
+context = canvas.getContext('2d');
 level = 1;
 requestAnimationFrame(mainLoop());
 }
 function mainLoop() {
+if (level === 1) pirates.length = 1;
+for (var i = 0, i < pirates.length; i++) {
+  
+}
 requestAnimationFrame(mainLoop());
+}
+function mouseClick (event) {
+  mouseClick = true;
+  click = [event.pageX,event.pageY];
+}
+function touchClick (event) {
+  event.preventDefault();
+  mouseClick = true;
+  click = [event.targetTouches[0].pageX, event.targetTouches[0].pageY];
 }
